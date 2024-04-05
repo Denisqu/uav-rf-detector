54f1ec32f0ac16169bfb279986f9056baf94f9a9
Denisque <denis2000.kuzik@gmail.com>
2024-04-05 12:34:56 +0300

    wip

--- uav-rf-detector-service/src/main.rs
+++ uav-rf-detector-service/src/main.rs
@@ -1,6 +1,13 @@
 use std::net::ToSocketAddrs;
+//use std::sync::mpsc;
+use tokio::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use tonic::transport::Server;
 use proto::detector_service_server;
+use std::time::Instant;
+use chrono;
+//use tonic::transport::{Certificate, Channel, ClientTlsConfig};
 
 mod proto;
 mod service;
@@ -11,13 +18,38 @@ mod service;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("../uav-rf-detector-client/files/proto/rf_detector.proto")?;
 
-    let detector_service = service::DetectorService {};
+    let (ml_tx, mut ml_rx) = mpsc::channel(1024);
 
+    // ML-executor thread
+    let ml_thread_handle = thread::spawn(move || {
+        let tx = ml_tx;
+        loop {
+            println!("sending current time from ml_thread...");
+            tx.try_send(chrono::prelude::Utc::now().to_string()).unwrap();
+            thread::sleep(Duration::new(1, 0));
+        }
+    });
+
+    // ML-Listener task
+    let ml_listener_handle = tokio::spawn(async move {
+       loop {
+            let result = ml_rx.recv().await;
+            println!("{}", result.unwrap_or("no result".to_string()));
+       }
+    });
+
+    // TODO: add sqlx postgress connection
+    // ...
+
+    let detector_service = service::DetectorService {};
     Server::builder()
         .add_service(detector_service_server::DetectorServiceServer::new(detector_service))
         .serve("127.0.0.1:50051".to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
+
+    ml_thread_handle.join().unwrap();
+    ml_listener_handle.await.unwrap();
 
     return Ok(());
 }
--- uav-rf-detector-service/src/service/mod.rs
+++ uav-rf-detector-service/src/service/mod.rs
@@ -1,23 +1,49 @@
+//use tokio::sync::mpsc::{Receiver, Sender};
+use crossbeam_channel;
 use tonic::{Request, Response, Status};
 use tonic::codegen::tokio_stream::wrappers::ReceiverStream;
 use crate::proto::{Detection, detector_service_server, Ping};
 use tokio::sync::mpsc;
 use tonic::codegen::Body;
 
-pub struct DetectorService {}
+pub struct DetectorService<T> {
+    ml_rx: crossbeam_channel::Receiver<T>,
+    ml_tx: crossbeam_channel::Sender<T>
+}
+
+impl<T> DetectorService<T> {
+    fn new() -> DetectorService<T> {
+        //let (tx, rx) = mpsc::channel(1024);
+        let (tx, rx) = crossbeam_channel::unbounded();
+        DetectorService {
+            ml_rx: rx,
+            ml_tx: tx
+        }
+    }
+
+    fn get_sender(&self) -> crossbeam_channel::Sender<T> {
+        return self.ml_tx.clone();
+    }
+}
 
 #[tonic::async_trait]
-impl detector_service_server::DetectorService for DetectorService {
+impl<T: std::marker::Send> detector_service_server::DetectorService for DetectorService<T> {
     type GetDetectionsStreamStream = ReceiverStream<Result<Detection, Status>>;
 
     async fn get_detections_stream(&self, request: Request<()>) -> Result<Response<Self::GetDetectionsStreamStream>, Status> {
         let (mut tx, rx) = mpsc::channel(120);
 
         tokio::spawn(async move {
+            loop {
+                tx.send(self.ml_rx.recv().await).await.unwrap();
+            }
+        });
+
+        /*tokio::spawn(async move {
             for i in 0..10 {
                 tx.send(Ok(Detection{value: "test".to_string(), timestamp: i.to_string()})).await.unwrap();
             }
-        });
+        });*/
 
         return Ok(Response::new(ReceiverStream::new(rx)))
     }
@@ -26,22 +52,3 @@ impl detector_service_server::DetectorService for DetectorService {
         return Ok(Response::new(Ping{value: request.into_inner().value}))
     }
 }
-
-/*
-#[tonic::async_trait]
-impl detector_service_server::DetectorService for DetectorService {
-    type startDetectionsStreamStream = ReceiverStream<Result<Detection, Status>>;
-
-    async fn start_detections_stream(&self, request: Request<()>) -> Result<Response<Self::startDetectionsStreamStream>, Status> {
-        let (mut tx, rx) = mpsc::channel(120);
-
-        tokio::spawn(async move {
-            for i in 0..10 {
-                tx.send(Ok(Detection::default())).await.unwrap();
-            }
-        });
-
-        return Ok(Response::new(ReceiverStream::new(rx)))
-    }
-}
- */
